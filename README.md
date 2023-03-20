bb6b2cc9b6a2501d5d8866b1d57b9fec3f6a2e8e
HENNESSYxie <hennessyxie@gmail.com>
2023-03-20 22:31:37 +0500

    lil fix

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # NumberPlateRecognition
+
+Number Plate Recognizing module by Khatamov Azizjon, using YOLOv5 and PaddleOCR.
+
+### Installation
+```commandline
+pip install -r requirements.txt
+```
+
+### Demo 
+In src.RecognizeNumberPlate.py change 'video' variable and set there path to your video(str).
+Run RecognizerNumberplate.py script:
+```commandline
+BASE_PATH=<path_to_project> python src/RecognizeNumberPlate.py 
+```
--- src/RecognizeNumberPlate.py
+++ src/RecognizeNumberPlate.py
@@ -12,7 +12,7 @@ visualizer = Visualizer()
 detector = YOLOv5PlateDetector(model=base_path + "/assets/models/detector/YOLOv5OpenVINO/yolov5.xml",
                                weights=base_path + "/assets/models/detector/YOLOv5OpenVINO/yolov5.bin",
                                output_layer_name="output0",
-                               score_threshold=0.7,
+                               score_threshold=0.5,
                                nms_threshold=0.7,
                                width=640,
                                height=640)
@@ -27,16 +27,18 @@ recognizer = PaddleOCR(model=base_path + "/assets/models/recognizer/PaddleOCR/pa
                        weights=base_path + "/assets/models/recognizer/PaddleOCR/paddleOCR.pdiparams",
                        output_layer_name='save_infer_model/scale_0.tmp_1',
                        score_threshold=0.8,
-                       nms_threshold=0.7,
+                       nms_threshold=0.77,
                        width=320,
                        height=48)
-decoder = VideoDecoder("/windows/car_number_plates/videos/second.mp4")
+video = "/windows/car_number_plates/videos/second.mp4"
+decoder = VideoDecoder(video)
 
 for frame in decoder.decode():
     detections = detector.predict(frame)
     for det in detections:
         cropped = frame[det.y1:det.y2, det.x1:det.x2]
         warped = aligner.predict(cropped)
+        warped = cv2.blur(warped, (1, 2))
         number = recognizer.predict(warped)
         frame = visualizer.visualize(image=frame, detection=det, number=number)
     cv2.imshow('Video', frame)
